5b5483b972997c2f2daa1627071751de4118ff1d
EwurasiD <dorcasacquah08@gmail.com>
2019-08-07 20:31:35 +0000

    config PORT to 3000 since it was undefined

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
+
+const config = {
+  PORT: 3000
+};
 
 module.exports = {
-    port: process.env.PORT,
-    apollo_server_url: process.env.APOLLO_SERVER_URL
-}
+  port: process.env.PORT,
+  apollo_server_url: process.env.APOLLO_SERVER_URL
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-//including modules 
-const express = require('express')
-const path = require('path')
+//including modules
+const express = require("express");
+const path = require("path");
 
 //including config variables
-const { port } = require('./config.js');
+const { PORT } = require("./config.js");
 
 //immediately invoked function to start server
 (() => {
-    //creating an express app
-    const app = express()
+  //creating an express app
+  const app = express();
 
-    //APP SETTINGS
-    //creating a link to our images,stylesheets and js
-    app.use('/public', express.static(path.join(__dirname, '/views/static')))
-    //setting up the view engine
-    app.set('view engine', 'ejs')
+  //APP SETTINGS
+  //creating a link to our images,stylesheets and js
+  app.use("/public", express.static(path.join(__dirname, "/views/static")));
+  //setting up the view engine
+  app.set("view engine", "ejs");
 
-    //including router function 
-    require('./routes')(app)
+  //including router function
+  require("./routes")(app);
 
-    //our web application serving webAPP at http://localhost:port
-    app.listen( {port}, () =>
-        console.log(`Serving app at http://localhost:${port}`)
-    )
-})()
+  //our web application serving webAPP at http://localhost:port
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+})();
