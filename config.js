ab11425bfd490391772f7c9c9b3d27df78df6604
Kenny-Rogers <ylodonu@gmail.com>
2019-08-28 05:36:39 +0000

    created send sms feature

--- config.js
+++ config.js
@@ -3,5 +3,9 @@ dotenv.config();
 
 module.exports = {
   port: process.env.PORT,
-  apollo_server_url: process.env.APOLLO_SERVER_URL
+  apollo_server_url: process.env.APOLLO_SERVER_URL,
+  sms_api_id: process.env.CLIENT_ID,
+  sms_api_secret: process.env.CLIENT_SECRET,
+  sms_api_url: process.env.SMS_API_URL,
+  sms_from: process.env.SMS_FROM
 };
